a057276ef746b265fcec9120585eda1a55d108b4
LeoAsahi <leonardo.bertonasco01@gmail.com>
2026-02-06 01:00:53 +0100

    Create color definitions

---
+++ 00/ex01/Colors.h
@@ -0,0 +1,10 @@
+#ifndef COLORS_H
+# define COLORS_H
+
+# define RESET       "\033[0m"
+# define BLACK       "\033[30m"
+# define RED         "\033[31m"
+# define GREEN       "\033[32m"
+# define YELLOW      "\033[33m"
+
+#endif
