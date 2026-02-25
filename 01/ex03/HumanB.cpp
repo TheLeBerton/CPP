d3b4d419bcaea3effb42d537b6eb81222a04f646
LeoAsahi <leonardo.bertonasco01@gmail.com>
2026-02-25 14:11:07 +0100

    feat(violence): add basic program

---
+++ 01/ex03/HumanB.cpp
@@ -0,0 +1,17 @@
+#include "HumanB.hpp"
+
+#include <iostream>
+
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
+
+HumanB::~HumanB() {}
+
+void HumanB::attack() const {
+  if (this->_weapon)
+    std::cout << this->_name << " attacks with their "
+              << this->_weapon->getType() << std::endl;
+  else
+    std::cout << this->_name << " has no weapon to attack with" << std::endl;
+}
+
+void HumanB::setWeapon(Weapon &weapon) { this->_weapon = &weapon; }
