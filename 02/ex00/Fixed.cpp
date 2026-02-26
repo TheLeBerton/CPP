fb878c27f3c316fa875b7a6b0ad71f2d777c496f
LeoAsahi <leonardo.bertonasco01@gmail.com>
2026-02-26 07:25:04 +0100

    feat(fixed): add print too all functions

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -1,20 +1,26 @@
 #include "Fixed.hpp"
 
+#include <iostream>
+
 Fixed::Fixed( void ) : _value(0)
 {
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed( const Fixed &src )
 {
+	std::cout << "Copy constructor called" << std::endl;
     *this = src;
 }
 
 Fixed::~Fixed( void )
 {
+	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed &Fixed::operator=( const Fixed &rhs )
 {
+	std::cout << "Copy assignment operator called" << std::endl;
     if (this != &rhs)
         this->_value = rhs._value;
     return *this;
@@ -22,10 +28,12 @@ Fixed &Fixed::operator=( const Fixed &rhs )
 
 int Fixed::getRawBits( void ) const
 {
+	std::cout << "getRawBits member function called" << std::endl;
     return this->_value;
 }
 
 void Fixed::setRawBits( int const raw )
 {
+	std::cout << "setRawBits member function called" << std::endl;
     this->_value = raw;
 }
