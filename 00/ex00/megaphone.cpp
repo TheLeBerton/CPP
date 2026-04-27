b17e5026e6eff657bbad8803017566505c2d61c9
TheLeBerton <theleberton@gmail.com>
2026-04-27 11:30:50 +0200

    refactor: make megaphone more readable and add manual tests
    closes #2

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,31 +1,42 @@
 #include <iostream>
 
-int	main(int argc, char **argv)
+static int	_handle_params( int argc ) ;
+static void	_print_str_uppercase( char *str ) ;
+
+int	main( int argc, char **argv )
 {
 	int	i;
-	int	j;
 
-	if (argc == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
+	if ( _handle_params( argc ) == 1 )
+		return ( 0 );
 	i = 1;
-	while (i < argc)
-	{
-		j = 0;
-		while (argv[i][j])
-		{
-			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-			{
-				std::cout << (char)(argv[i][j] - 32);
-			}
-			else
-				std::cout << argv[i][j];
-			j++;
-		}
+	while ( i < argc ) {
+		_print_str_uppercase( argv[ i ] );
 		i++;
 	}
 	std::cout << std::endl;
 	return (0);
+}
+
+static int	_handle_params( int argc ) {
+	if ( argc == 1 ) { 
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return ( 1 );
+	}
+	return ( 0 );
+}
+
+static void	_print_char_uppercase( char c ) {
+	if ( c >= 'a' && c <= 'z' )
+		c = c - 32;
+	std::cout << c;
+}
+
+static void	_print_str_uppercase( char *str ) {
+	int	i = 0;
+	
+	while ( str[ i ] ) {
+		_print_char_uppercase( str[ i ] );
+		i++;
+	}
 }
